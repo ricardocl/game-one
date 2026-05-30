00e67514951656d9fd7f1089f586a26f3211d467
Ricardo Ramirez <ricardo.ramirezw@gmail.com>
2026-05-30 15:21:59 -0400

    update

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,22 @@
 extends CharacterBody2D
 
-@export var velocidad := 200.0
+@export var velocidad := 150.0
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	anim.play("idle")
 
 func _physics_process(delta):
 	var direccion = Vector2.ZERO
-
-	# Movimiento horizontal
 	if Input.is_action_pressed("ui_right"):
 		direccion.x += 1
-
 	if Input.is_action_pressed("ui_left"):
 		direccion.x -= 1
-
-	# Movimiento vertical
 	if Input.is_action_pressed("ui_down"):
 		direccion.y += 1
-
 	if Input.is_action_pressed("ui_up"):
 		direccion.y -= 1
-
-	# Evita movimiento más rápido en diagonal
 	direccion = direccion.normalized()
-
 	velocity = direccion * velocidad
-
 	move_and_slide()
